e038592c0b3bd00b4c0646e5c65fa642124c858b
Sophanit-Keo <sophanitkeo1803@gmail.com>
2026-09-19 19:46:58 +0700

    login screen

--- app/src/main/java/com/example/appconnect/MainActivity.java
+++ app/src/main/java/com/example/appconnect/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.appconnect;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.appconnect.databinding.ActivityMainBinding;
 
@@ -15,45 +23,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.ime());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.loginScreen.setOnClickListener(v -> hideKeyboard());
+
         binding.btnLogin.setOnClickListener(v -> {
-            loginUser();
+
         });
 
         binding.txtForgotPassword.setOnClickListener(v -> {
 
         });
+
+        addTextInputListener();
     }
 
-    private void loginUser() {
+    private void hideKeyboard() {
+        // Find the currently focused view, so we can grab the correct window token from it.
+        View view = getCurrentFocus();
 
-        String email =
-                binding.editEmail.getText() != null
-                        ? binding.editEmail.getText().toString().trim()
-                        : "";
-
-        String password =
-                binding.editPassword.getText() != null
-                        ? binding.editPassword.getText().toString()
-                        : "";
-
-        binding.emailLayout.setError(null);
-        binding.passwordLayout.setError(null);
-
-        if (email.isEmpty()) {
-            binding.emailLayout.setError(
-                    "Enter your email or username"
-            );
-            return;
+        // If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(this);
         }
 
-        if (password.isEmpty()) {
-            binding.passwordLayout.setError(
-                    "Enter your password"
-            );
-            return;
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+    }
+
+    private void validateButtonState() {
+        boolean isEmailEmpty = binding.editEmail.getText() == null || binding.editEmail.getText().toString().trim().isEmpty();
+        boolean isPasswordEmpty = binding.editPassword.getText() == null || binding.editPassword.getText().toString().trim().isEmpty();
+
+        binding.btnLogin.setEnabled(!isEmailEmpty && !isPasswordEmpty);
+    }
+
+    private void addTextInputListener() {
+        TextWatcher handleTextChange = new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                validateButtonState();
+            }
+        };
+
+        binding.editEmail.addTextChangedListener(handleTextChange);
+        binding.editPassword.addTextChangedListener(handleTextChange);
     }
 }
